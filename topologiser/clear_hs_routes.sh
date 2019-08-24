1ddba60b9ac0d045e2571ebdf6e54e7f996da0de
Valer Cara <blk@griminal.net>
2019-08-24 03:30:20 +0300

    Move to dockerized postgres: ensure topologiser maintains route to postgres

--- topologiser/clear_hs_routes.sh
+++ topologiser/clear_hs_routes.sh
@@ -8,5 +8,9 @@ NETWORK=${GW/%0.1/0.0}
 
 ip ro flush all
 ip ro add $GW dev eth0
+
+# Postgres is dockerised, running in the same network. Ensure we don't kill connectivity.
+ip ro add $POSTGRES_HOST dev eth0
+
 ip ro add blackhole $NETWORK/16
 ip ro add default via $GW
